05accfa8c39fdc2d8a4668884e8bf581522c985d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 10:26:48 +0100

    Refactor my app.js file and seperated my server file into server.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,11 @@ const express = require("express");
 const router = express.Router();
 const tourCtrl = require("./../controllers/tourCtrl");
 
+router.param("id", (req, res, next, val) => {
+  console.log(`Tour Id is :${val}`);
+  next();
+});
+
 router
   .route("/")
   .get(tourCtrl.getAllTours)
