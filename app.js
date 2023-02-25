b4b9a957206735233c1ed7775eecd6c857186b42
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 03:25:52 +0100

    Created a more precise error handling for invaild ID in getting params in Nodejs

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ app.post('/api/v1/tours', (req, res) => {
 // Get a tour
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = parseInt(req.params.id);
+  const tour = tours.find(newId => newId.id === id);
 
-  if (id > tours.length) {
+  if (!tour) {
     return res.status(httpStatus.NOT_FOUND).json({
       status: "fail",
       message: "Invalid ID"
     });
   }
-  const tour = tours.find(newId => newId.id === id);
 
   console.log(req.params);
   res.status(httpStatus.OK).json({
