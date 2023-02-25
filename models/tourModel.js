cbcc005cc699cc269501f50adb2a5755fd28372b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 20:19:19 +0100

    Creating more tourModel properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,61 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true
-  },
-  duration: {
-    type: Number,
-    required: [true, "A tour must have a duration"]
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "A tour must have a group size"]
-  },
-  difficulty: {
-    type: String,
-    required: [true, "A tour must have a difficulty"]
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"]
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"]
+    },
 
-  ratingsAverage: {
-    type: Number,
-    default: 2
-  },
+    ratingsAverage: {
+      type: Number,
+      default: 2
+    },
 
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    default: 100
-  },
-  priceDiscount: Number,
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      default: 100
+    },
+    priceDiscount: Number,
 
-  summary: {
-    type: String,
-    trim: true
+    summary: {
+      type: String,
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have an imageCover"]
+    },
+    images: [String],
+    createAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have an imageCover"]
-  },
-  images: [String],
-  createAt: {
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date]
-});
+  { timestamps: true }
+);
 
 const Tour = mongoose.model("Tour", tourSchema);
 module.exports = Tour;
