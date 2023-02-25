c4bafcebe9f807afc16eed4aa5449ba1294b8b42
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 12:21:31 +0100

    Error fixed in Port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const dotenv = require("dotenv");
-const app = require("./app");
 
 dotenv.config({
-  path
-})
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port : ${port}..`);
+  path: "./config.env"
+});
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port : ${PORT}..`);
 });
