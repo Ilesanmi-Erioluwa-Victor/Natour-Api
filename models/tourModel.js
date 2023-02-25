2712b120cfc25aeec2060367d931b9f5ba6cad4e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 19:56:40 +0100

    Creating more tourModel properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,18 @@ const tourSchema = new mongoose.Schema({
     required: [true, "Name is required"],
     unique: true
   },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"]
+  },
+  difficulty: {
+     
+  },
+  
   rating: {
     type: Number,
     default: 2
