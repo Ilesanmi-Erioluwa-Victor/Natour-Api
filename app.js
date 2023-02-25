862045f9e17c42b8656bb644526e75e8d71e3dc9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 02:36:09 +0100

    Fetched internal data from my dev-data

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+// Get all tours...
 app.get('/api/v1/tours', (req, res) => {
   res.status(httpStatus.OK).json({
     status: 'success',
     result : tours.length,
     data: { tours }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+
 });
 
 const port = 3000;
