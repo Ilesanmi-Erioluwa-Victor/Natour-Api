9660090c732b911ac2ef080fd0ab998411510232
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 03:46:09 +0100

    Refactoring my routes code

--- app.js
+++ app.js
@@ -3,56 +3,64 @@ const httpStatus = require('http-status');
 const express = require('express');
 
 const app = express();
-
+// Middlewares
 app.use(express.json());
-
-
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// Get all tours...
-app.get('/api/v1/tours', (req, res) => {
+
+const getAllTours = (req, res) => {
   res.status(httpStatus.OK).json({
     status: 'success',
-    result : tours.length,
-    data: { tours }
+    result: tours.length,
+    data: { tours },
   });
-});
+};
 
-
-// Create new Tour
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(httpStatus.CREATED).json({
-      status: "success",
-      data: {
-        tour : newTour
-      }
-   })
-  });
-});
-
-// Get a tour
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   const id = parseInt(req.params.id);
-  const tour = tours.find(newId => newId.id === id);
+  const tour = tours.find((newId) => newId.id === id);
 
   if (!tour) {
     return res.status(httpStatus.NOT_FOUND).json({
-      status: "fail",
-      message: "Invalid ID"
+      status: 'fail',
+      message: 'Invalid ID',
     });
   }
-
+  
   res.status(httpStatus.OK).json({
     status: 'success',
     data: { tour },
   });
-});
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(httpStatus.CREATED).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+c
+// Get all tours...
+app.get('/api/v1/tours',getAllTours);
+// Get a tour
+app.get('/api/v1/tours/:id', getTour);
+// Create new Tour
+app.post('/api/v1/tours', createTour);
+
 
 // Update a tour with Patch method
 app.patch("/api/v1/tours/:id", (req, res) => {
