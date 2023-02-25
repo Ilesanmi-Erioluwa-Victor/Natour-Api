ac20dc4d0c1c114152b30a4a7ce0012793848df5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 18:08:27 +0100

    Refactors all my codes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,14 +4,13 @@ const router = express.Router();
 
 const ValidateId = require("../middlewares/ValidateId");
 const tourCtrl = require("./../controllers/tourCtrl");
-const { CheckBody } = require("../middlewares/Checkbody");
 
 router.param("id", ValidateId);
 
 router
   .route("/")
   .get(tourCtrl.getAllTours)
-  .post(CheckBody, tourCtrl.createTour);
+  .post(tourCtrl.createTour);
 
 router
   .route("/:id")
