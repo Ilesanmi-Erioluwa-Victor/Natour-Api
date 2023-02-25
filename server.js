5f2b87ae1e050fd78c07823fe6658a4eff8cce4a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-25 11:49:33 +0100

    Working on configuration of .env files

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const dotenv = require("dotenv");
 const app = require("./app");
 
+dotenv.config({
+  path
+})
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port : ${port}..`);
