cb39162986963af9a75cef88b4320fd035cb3497
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-02-27 18:19:15 +0100

    Working on sort with some addition filtering

--- controllers/tourCtrl.js
+++ controllers/tourCtrl.js
@@ -36,9 +36,11 @@ exports.getAllTours = async (req, res) => {
 
     // SORTING
     if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      console.log(sortBy);
       query = query.sort(req.query.sort);
       //1) if two tours has same price, then
-      //2) sort("price ratingsAverage")
+      //2) sort("price ratingsAverage");
     }
     // EXECUTE THE QUERY
     const tours = await query;
