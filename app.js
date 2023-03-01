e05cd198407452b201e33d2f1a3bd2bb5584edb5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-01 10:07:44 +0100

    Working on error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const httpStatus = require("http-status");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./Utils/appError");
 
 const app = express();
 
@@ -25,15 +26,7 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  // res.status(httpStatus.NOT_FOUND).json({
-  //   status: "fail",
-  //   message: `Can't find ${req.originalUrl} on this server!!`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!!`);
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
 });
 
 app.use((err, req, res, next) => {
