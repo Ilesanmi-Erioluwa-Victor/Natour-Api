d4640cf879774ea4955bdb02cedb2550438b128f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 03:56:59 +0100

    Working on error mode code

--- controllers/errorCtrl.js
+++ controllers/errorCtrl.js
@@ -1,6 +1,11 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+  } else if (process.env.NODE_ENV === "production") {
+  }
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message
