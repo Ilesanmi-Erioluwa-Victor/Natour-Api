9db39900a168da8f9abe8b9fa78ad9d3b9741b94
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 03:31:42 +0100

    Working on custom catchAsync Error handler

--- controllers/tourCtrl.js
+++ controllers/tourCtrl.js
@@ -46,42 +46,28 @@ exports.getTour = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const tour = await Tour.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    res.status(httpStatus.OK).json({
-      status: "success",
-      data: {
-        tour
-      }
-    });
-  } catch (error) {
-    res.status(httpStatus.BAD_REQUEST).json({
-      status: "fail",
-      message: error
-    });
-  }
-};
+exports.updateTour = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const tour = await Tour.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  res.status(httpStatus.OK).json({
+    status: "success",
+    data: {
+      tour
+    }
+  });
+});
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Tour.findByIdAndDelete(id);
-    res.status(httpStatus.NO_CONTENT).json({
-      status: "success",
-      data: { tour: null }
-    });
-  } catch (error) {
-    res.status(httpStatus.BAD_REQUEST).json({
-      status: "fail",
-      message: error
-    });
-  }
-};
+exports.deleteTour = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  await Tour.findByIdAndDelete(id);
+  res.status(httpStatus.NO_CONTENT).json({
+    status: "success",
+    data: { tour: null }
+  });
+});
 
 exports.getToursStats = async (req, res) => {
   try {
