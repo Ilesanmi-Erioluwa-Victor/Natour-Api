3c12043406d637ccd2553ad2caf364a85e4fe183
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 04:13:14 +0100

    Working on error mode code

--- controllers/errorCtrl.js
+++ controllers/errorCtrl.js
@@ -8,12 +8,21 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
+  // Operational error, trusted error, send to client
+
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
   } else {
+    // Programming or unknown error, don't leak details
+
+    // 1)Log the error
+    console.error("Error : ", err);
+
+    // 2) Send generic message
+    
     res.status(500).json({
       status: "error",
       message: "Something went wrong, try again"
