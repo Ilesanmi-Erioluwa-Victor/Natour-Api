81d783d66b50e31855127fa3b6bdcf86d7f48f1e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-05 09:08:42 +0100

    a bug in errorCtrl, custom error not working as expected

--- controllers/errorCtrl.js
+++ controllers/errorCtrl.js
@@ -1,8 +1,22 @@
 const AppError = require("../Utils/appError");
 
 const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path} : ${err.value}`;
-  return new AppError(``);
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
 };
 
 const sendErrorDev = (err, res) => {
@@ -15,28 +29,29 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  // Operational error, trusted error, send to client
-
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
-  } else {
-    // Programming or unknown error, don't leak details
 
-    // 1)Log the error
-    console.error("Error : ", err);
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("ERROR 💥", err);
 
     // 2) Send generic message
     res.status(500).json({
       status: "error",
-      message: "Something went wrong, try again"
+      message: "Something went very wrong!"
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
@@ -44,7 +59,12 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
-    // if(error.name ==== "CastError") error = handleCastErrorDB(error)
-    sendErrorProd(err, res);
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
