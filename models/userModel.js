84b7d34238c192e453efc75b6ef8d903b06209ec
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-08 01:24:39 +0100

    working on createPasswordResetToken

--- models/userModel.js
+++ models/userModel.js
@@ -83,10 +83,14 @@ userSchema.methods.changePasswordAfter = function(JWTTimeStamps) {
 userSchema.methods.createPasswordResetToken = function() {
   const resetToken = crypto.randomBytes(32).toString("hex");
 
-  crypto
+  this.passwordResetToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 const User = mongoose.model("User", userSchema);
 module.exports = User;
