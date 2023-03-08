0e319e8b0122e9bd7eaa1a24ac8dcf8677f03404
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-08 02:06:04 +0100

    working on nodemailer

--- Utils/email.js
+++ Utils/email.js
@@ -12,5 +12,9 @@ const sendEmail = options => {
     //if you want to use gmail,  Activate in gmail "less secure app" option
   });
   // 2) Define the email options
+    const mailOptions = {
+        from: "Ilesanmi Erioluwa <ilesanmierioluwavictor2gmail.com>",
+        to : 
+    }
   // 3) Actually send the email..
 };
