24b03ce157d9f16de16b526e48ad58a767e7b2a4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-03-08 01:52:59 +0100

    working on nodemailer

--- Utils/email.js
+++ Utils/email.js
@@ -2,9 +2,11 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = options => {
   // 1) Create a transporter
-    const transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth : 
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USERName
+    }
   });
   // 2) Define the email options
   // 3) Actually send the email..
