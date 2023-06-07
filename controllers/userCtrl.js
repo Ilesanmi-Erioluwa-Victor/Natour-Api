f69f6f05d2ad5bc9b57a16e60ae6573fa2686ba4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-07 09:37:14 +0100

    Working on userCtrl route

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,6 +2,14 @@ const httpStatus = require("http-status");
 const User = require("../models/userModel");
 const catchAsync = require("../Utils/catchAsync");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   res.status(httpStatus.CREATED).json({
