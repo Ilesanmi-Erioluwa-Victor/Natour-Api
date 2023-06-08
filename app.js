936352a064a1b5c67a0b9569ac28b16fa982c7b3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-08 22:51:30 +0100

    Working on app,js (helmet function)

--- app.js
+++ app.js
@@ -38,6 +38,23 @@ app.use(express.json({ limit: "10kb" }));
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price"
+    ]
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 // For getting time
 app.use((req, res, next) => {
