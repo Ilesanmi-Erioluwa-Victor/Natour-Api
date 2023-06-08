4f914ed8704ef668c1f4e37362cae1789c85a5f5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-08 22:29:43 +0100

    Working on app,js (rateLimi function)

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+const Limiter = rateLimit({})
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 // For getting time
