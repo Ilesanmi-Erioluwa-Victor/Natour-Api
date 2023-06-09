559f2200612f27c6be833e7e54bfd3611243cf56
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-09 22:54:31 +0100

    working on tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -86,7 +86,20 @@ const tourSchema = new mongoose.Schema(
       coordinates: [Number],
       address: String,
       description: String
-    }
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"]
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   {
     timestamps: true,
