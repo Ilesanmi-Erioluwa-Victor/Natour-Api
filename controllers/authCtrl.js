1c47c8ec4e9be3311ffbd0ce6ceebfea6a9561ea
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-09 22:35:40 +0100

    First commit of the day

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -14,7 +14,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   const user = await User.create({
     name: req.body.name,
     email: req.body.email,
-    photo: req.body.photo,
+    // photo: req.body.photo,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });
