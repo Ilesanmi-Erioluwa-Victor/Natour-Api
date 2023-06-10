40e57467eaed7ea6a35924fb0d1f8d624e4c9928
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-10 21:39:41 +0100

    Working on reviewModel

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
   review: {
-    type: String
+    type: String,
+    required: [true, "Review can not be empty"]
   }
 });
