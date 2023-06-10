b7737f164d3d9a68aa5b1e7173bf6f4fa8d050ca
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-10 22:23:34 +0100

    Working on reviewModel contoller

--- controllers/reviewCtrl.js
+++ controllers/reviewCtrl.js
@@ -13,4 +13,6 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {});
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+});
