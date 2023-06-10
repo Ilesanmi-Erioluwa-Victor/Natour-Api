510f2f948c081df28ad52dda18369ef65963ea12
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-10 23:04:16 +0100

    Working on reviewModel contoller

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const reviewCtrl = require("../controllers/reviewCtrl");
-
+const authCtrl = require("../controllers/authCtrl");
 const router = express.Router();
 
 router
   .route("/")
   .get(reviewCtrl.getAllReviews)
-  .post(reviewCtrl.createReview);
+  .post(authCtrl.protect, reviewCtrl.createReview);
 
 module.exports = router;
