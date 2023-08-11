f24cd94b972eec72d41e815967adbd7a4fba7a51
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-11 01:20:56 +0100

    facing weird bug

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,21 +1,27 @@
 const express = require("express");
 
-// const router = express.Router();
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 
 // const ValidateId = require("../middlewares/ValidateId");
 const tourCtrl = require("./../controllers/tourCtrl");
-// const reviewRoute = require("./../controllers/reviewCtrl");
+const reviewRoute = require("./../controllers/reviewCtrl");
 const authCtrl = require("./../routes/reviewRoutes");
 // router.param("id", ValidateId);
-// router.use("/:tourId/reviews", reviewRoute);
+
+router.use("/:tourId/reviews", reviewRoute);
 
 router
-  .route("/top-5-cheapTours")
+  .route("/top-5-cheap")
   .get(tourCtrl.aliasesTopTours, tourCtrl.getAllTours);
 
-router.route("/tours-stats").get(tourCtrl.getToursStats);
-router.route("/monthly-plan/:year").get(tourCtrl.getMonthlyPlan);
+router.route("/tour-stats").get(tourCtrl.getTourStats);
+router
+  .route("/monthly-plan/:year")
+  .get(
+    authCtrl.protect,
+    authCtrl.restrictTo("admin", "lead-guide", "guide"),
+    tourCtrl.getMonthlyPlan
+  );
 
 router
   .route("/")
